f670f147f271af18f4c2cf7825eb4156c10263cc
Clement Habinshuti <hello@habbes.xyz>
2017-07-02 00:14:51 +0300

    Establish websocket connection

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const WebSocket = require('ws');
 const PORT = process.env.PORT || 3000;
 
 // Run web server to server static html page
@@ -13,4 +14,14 @@ const server = http.createServer((req, res) => {
 
 server.listen(PORT, () => {
     console.log("Server listening on", PORT);
+});
+
+// WebSocket server
+
+const wss = new WebSocket.Server({ server });
+wss.on('connection', conn => {
+    console.log('client connected');
+    conn.on('message', data => {
+        console.log('message received %j', data);
+    });
 });
