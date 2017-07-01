e2d454dd37e16c056540b88d5afd534bf96febcf
Clement Habinshuti <hello@habbes.xyz>
2017-07-01 22:56:34 +0300

    Serve static html page

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,16 @@
-console.log("Web server coming soon...");
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const PORT = process.env.PORT || 3000;
+
+// Run web server to server static html page
+
+const server = http.createServer((req, res) => {
+    const stream = fs.createReadStream(path.join(__dirname, '../web/index.html'));
+    res.writeHead(200, {'content-type': 'text/html'});
+    stream.pipe(res);
+});
+
+server.listen(PORT, () => {
+    console.log("Server listening on", PORT);
+});
