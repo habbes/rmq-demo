c28a8a8e0c6adf55bf192d963c632e377b6e6b3b
Clement Habinshuti <hello@habbes.xyz>
2017-07-02 00:44:45 +0300

    Handle id websocket message on both client and server

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const crypto = require('crypto');
 const WebSocket = require('ws');
 const PORT = process.env.PORT || 3000;
+const SERVER_ID = generateServerId();
 
 // Run web server to server static html page
 
@@ -13,15 +15,67 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-    console.log("Server listening on", PORT);
+    console.log("Server %s listening on port %d", SERVER_ID, PORT);
 });
 
 // WebSocket server
 
 const wss = new WebSocket.Server({ server });
 wss.on('connection', conn => {
-    console.log('client connected');
     conn.on('message', data => {
-        console.log('message received %j', data);
+        const msg = JSON.parse(data);
+        handleMessage(msg);
     });
+    sendMessage(conn, buildIdMessage(SERVER_ID));
 });
+
+
+/**
+ * generates a random id to assign to server instance
+ * @return {String}
+ */
+function generateServerId () {
+    return crypto.randomBytes(12).toString('hex');
+}
+
+// CLIENT MESSAGES
+
+function sendMessage (conn, msg) {
+    conn.send(JSON.stringify(msg));
+}
+
+/**
+ * message used to identify the server instance
+ * @param {String} id unique server id
+ * @return {Object}
+ */
+function buildIdMessage (id) {
+    return {
+        type: 'id',
+        serverId: id
+    };
+}
+
+// Message handlers
+
+/**
+ * handles a message from a client
+ * based on its type
+ * @param {Object} msg 
+ */
+function handleMessage (msg) {
+    switch (msg.type) {
+        case 'id':
+            return handleIdMessage(msg);
+        default:
+            console.warn("Unhandled message", msg);
+    }
+}
+
+/**
+ * handles and id message from a client
+ * @param {Object} msg 
+ */
+function handleIdMessage (msg) {
+    console.log('Client connected:', msg.clientId);
+}
